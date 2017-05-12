effdb71d30faf726b93a14c1cf67711294da4b7d
caroquintana <caro.quintana.n@gmail.com>
2017-05-12 12:37:17 -0400

    añadiendo estilo e imagen

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-//Rescatar datos usuario y producto escogido
-var nomb = document.getElementById("nombre").value;
-var apell = document.getElementById("apellido").value;
-var email = document.getElementById("email").value;
-var direc = document.getElementById("direccion").value;
-var tel = document.getElementById("telefono").value;	
-var opciónTe = document.getElementById("datosusuario").value;
-		
-
-mostrarProducto.innerHTML += opciónTe.value;
-opciónTe.value = " ";	
-
+//Rescatar el comentario del usuario
+function mostrarData() {
+	//Rescatar datos usuario y producto escogido
+	var nomb = document.getElementById("nombre").value;
+	var apell = document.getElementById("apellido").value;
+	var email = document.getElementById("email").value;
+	var direc = document.getElementById("direccion").value;
+	var tel = document.getElementById("telefono").value;	
+	var opciónTe = document.getElementById("datosusuario").value;
+	
+	var reflejarDatos = document.getElementById("cliente");
+	reflejarDatos.innerHTML +=  nomb;	
 }
-
