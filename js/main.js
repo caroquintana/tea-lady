30cae5ce8eb6623cdeb5fca501889e39793bd3cb
caroquintana <caro.quintana.n@gmail.com>
2017-05-12 11:15:25 -0400

    avance js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+//Rescatar datos usuario y producto escogido
+
+var nomb = document.getElementById("nombre").value;
+var apell = document.getElementById("apellido").value;
+var email = document.getElementById("email").value;
+var direc = document.getElementById("direccion").value;
+var tel = document.getElementById("telefono").value;	
+var opciónTe = document.getElementById("datosusuario").value;
+		
+
+mostrarProducto.innerHTML += opciónTe.value;
+opciónTe.value = " ";	
+
+}
+
