33e7b51f88c3b286a9dfeef8358501ed92433bda
caroquintana <caro.quintana.n@gmail.com>
2017-05-12 13:15:13 -0400

    correcion js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-//Rescatar el comentario del usuario
-function mostrarData() {
-	//Rescatar datos usuario y producto escogido
+function mostrarData(){
 	var nomb = document.getElementById("nombre").value;
 	var apell = document.getElementById("apellido").value;
-	var email = document.getElementById("email").value;
+	var mail = document.getElementById("email").value;
 	var direc = document.getElementById("direccion").value;
-	var tel = document.getElementById("telefono").value;	
-	var opciónTe = document.getElementById("datosusuario").value;
-	
-	var reflejarDatos = document.getElementById("cliente");
-	reflejarDatos.innerHTML +=  nomb;	
+	var fono = document.getElementById("telefono").value;
+	var opcionTe = document.getElementById("teescogido");
+	var imprimirDatos = document.getElementById("clientes");
+
+
+	imprimirDatos.innerHTML = "<br><strong>Nombre: </strong>" + nomb + "<br><strong>Apellido: </strong>" + apell + "<br><strong>Dirección: </strong>" + direc + "<br><strong>Correo: </strong>" + mail + "<br><strong>Teléfono: </strong>" + fono + "<br><strong>Té seleccionado: </strong>" + teescogido;
 }
