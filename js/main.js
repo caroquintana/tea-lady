d39359c433f0d97572b4844ccb0f241a146f7af2
caroquintana <caro.quintana.n@gmail.com>
2017-05-12 13:22:34 -0400

    js corregido

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ function mostrarData(){
 	var mail = document.getElementById("email").value;
 	var direc = document.getElementById("direccion").value;
 	var fono = document.getElementById("telefono").value;
-	var opcionTe = document.getElementById("teescogido");
+	var opcionTe = document.getElementById("teescogido").value;
 	var imprimirDatos = document.getElementById("clientes");
 
 
-	imprimirDatos.innerHTML = "<br><strong>Nombre: </strong>" + nomb + "<br><strong>Apellido: </strong>" + apell + "<br><strong>Dirección: </strong>" + direc + "<br><strong>Correo: </strong>" + mail + "<br><strong>Teléfono: </strong>" + fono + "<br><strong>Té seleccionado: </strong>" + teescogido;
+	imprimirDatos.innerHTML = "<br><strong>Nombre: </strong>" + nomb + "<br><strong>Apellido: </strong>" + apell + "<br><strong>Dirección: </strong>" + direc + "<br><strong>Correo: </strong>" + mail + "<br><strong>Teléfono: </strong>" + fono + "<br><strong>Té seleccionado: </strong>" + opcionTe;
 }
